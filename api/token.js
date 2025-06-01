db76d2245e5f0116307628f40ec87488fc82820a
jaime.wong <jaime.wong@ehelply.com>
2025-06-01 17:36:16 -0600

    Initial commit

---
+++ api/token.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  const client_id = process.env.SPOTIFY_CLIENT_ID;
+  const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+  const auth = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${auth}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await response.json();
+
+  if (data.access_token) {
+    res.status(200).json({ token: data.access_token });
+  } else {
+    res.status(500).json({ error: "Failed to get token" });
+  }
+}
