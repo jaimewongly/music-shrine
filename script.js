af2c6db2e11c02dde8c34a548b00f23a6fbdd9bc
jaime.wong <jaime.wong@ehelply.com>
2025-06-01 18:41:53 -0600

    Query Spotify API for albums

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// make glow stronger
-
-// make the glows randomly timed for each bulb
 const lightPositions = [
   [18, 37.25],
   [22, 42.5],
@@ -28,17 +25,61 @@ lightPositions.forEach(([top, left]) => {
   const bulb = document.createElement("span");
   bulb.classList.add("bulb");
   bulb.classList.add("yellow");
-
-  // Random animation delay and duration
-  //const delay = Math.random() * 3; // delay between 0-3 seconds
-  const delay = 0;
-  //const duration = 2 + Math.random() * 2; // duration between 1-3 seconds
   const duration = 3;
-
   bulb.style.top = `${top}%`;
   bulb.style.left = `${left}%`;
-  bulb.style.animationDelay = `${delay}s`;
   bulb.style.animationDuration = `${duration}s`;
-
   document.body.appendChild(bulb);
 });
+
+function submitArtist() {
+  const artist = document.getElementById("artist-input").value;
+  fetchAlbums(artist);
+}
+
+async function fetchAlbums(artistName) {
+  const tokenRes = await fetch("/api/token");
+  const tokenData = await tokenRes.json();
+  const { token } = tokenData;
+  console.log("Token response data:", tokenData);
+  console.log("Sending token to Spotify:", token);
+
+  const searchRes = await fetch(
+    `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+      artistName
+    )}&type=artist`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const searchData = await searchRes.json();
+  const artistId = searchData.artists.items[0]?.id;
+  if (!artistId) {
+    console.error("Artist not found");
+    return;
+  }
+
+  const albumsRes = await fetch(
+    `https://api.spotify.com/v1/artists/${artistId}/albums?include_groups=album&limit=10`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const albumsData = await albumsRes.json();
+  const firstAlbumCover = albumsData.items[0]?.images[0]?.url;
+
+  if (firstAlbumCover) {
+    document.getElementById(
+      "album-placeholder"
+    ).style.backgroundImage = `url(${firstAlbumCover})`;
+    document.getElementById("album-placeholder").style.backgroundSize = "cover";
+    document.getElementById("album-placeholder").style.backgroundPosition =
+      "center";
+  } else {
+    console.warn("No album covers found");
+  }
+}
